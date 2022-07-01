195eab1057647414d2f808d0b184fbdf5f0042e6
Brs545 <baris.candogan@hotmail.com>
2022-07-01 15:18:05 +0300

    adding bike component

---
+++ screens/BikeScreen.js
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import Bike from '../components/Bike';
+const BikeScreen = () => {
+  return (
+    <SafeAreaView style={tw`h-full bg-gray-400 `}>
+      <View>
+        <View>
+          <Bike />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default BikeScreen;
+
+const styles = StyleSheet.create({});
