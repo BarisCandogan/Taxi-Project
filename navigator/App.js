b4d08a476d9a7dd1ce316f37db69ea9b069616cb
Brs545 <baris.candogan@hotmail.com>
2022-07-01 15:25:56 +0300

    Navigator

--- navigator/App.js
+++ navigator/App.js
@@ -11,7 +11,6 @@ import {
 import MapScreen from '../screens/CarScreen';
 import HomeDisplay from '../screens/HomeDisplay';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BikeScreen from '../screens/BikeScreen';
